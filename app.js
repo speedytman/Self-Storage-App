1d5b7f7171d1124f7a49db184bce6e7f633e0fcd
speedytman <wdcoker3@gmail.com>
2021-01-13 21:46:04 -0500

    Setting up Node.js

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var path = require("path");
+
+
+var app = express();
+
+
+app.set("port", process.env.PORT || 3000);
+
+app.listen(app.get("port"),function(){
+    console.log("Server started on port " + app.get("port"));
+});
